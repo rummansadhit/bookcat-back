79098471bf18fddb1f32973ef70b6e8f8ba4b7d1
Rumman Sadhit <rummansadhit@outlook.com>
2023-07-15 13:56:25 -0400

    first commit

---
+++ model/book.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IBook extends Document {
+  title: string;
+  author: string;
+  genre: string;
+  publicationDate: Date;
+}
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  publicationDate: { type: Date, required: true },
+});
+
+const Book = mongoose.model<IBook>('Book', bookSchema);
+
+export default Book;
---
+++ server.ts
@@ -0,0 +1,98 @@
+import express, { Request, Response, Application } from 'express';
+import mongoose, { Document } from 'mongoose';
+import bodyParser from 'body-parser';
+import Book, { IBook } from './model/book';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+// Set up middleware
+app.use(bodyParser.json());
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+const mongodb = process.env.MONGOURL || 'mongodb://localhost:27017/books';
+
+
+
+// Connect to MongoDB
+mongoose.connect(mongodb)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
+  app.post('/books', (req: Request, res: Response) => {
+    const book: IBook = new Book(req.body);
+    book.save()
+      .then((result: IBook) => {
+        res.json(result);
+      })
+      .catch((error: Error) => {
+        res.status(500).json({ error: 'Error creating book' });
+      });
+  });
+  
+  // Read all books
+  app.get('/books', (req: Request, res: Response) => {
+    Book.find()
+      .then((books: IBook[]) => {
+        res.json(books);
+      })
+      .catch((error: Error) => {
+        res.status(500).json({ error: 'Error retrieving books' });
+      });
+  });
+  
+  // Read a single book
+  app.get('/books/:id', (req: Request, res: Response) => {
+    Book.findById(req.params.id)
+      .then((book: IBook | null) => {
+        if (book) {
+          res.json(book);
+        } else {
+          res.status(404).json({ error: 'Book not found' });
+        }
+      })
+      .catch((error: Error) => {
+        res.status(500).json({ error: 'Error retrieving book' });
+      });
+  });
+  
+  // Update a book
+  app.put('/books/:id', (req: Request, res: Response) => {
+    Book.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then((book: IBook | null) => {
+        if (book) {
+          res.json(book);
+        } else {
+          res.status(404).json({ error: 'Book not found' });
+        }
+      })
+      .catch((error: Error) => {
+        res.status(500).json({ error: 'Error updating book' });
+      });
+  });
+  
+  // Delete a book
+  app.delete('/books/:id', (req: Request, res: Response) => {
+    Book.findByIdAndDelete(req.params.id)
+      .then((book: IBook | null) => {
+        if (book) {
+          res.json(book);
+        } else {
+          res.status(404).json({ error: 'Book not found' });
+        }
+      })
+      .catch((error: Error) => {
+        res.status(500).json({ error: 'Error deleting book' });
+      });
+  });
